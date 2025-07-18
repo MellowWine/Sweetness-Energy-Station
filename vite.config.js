a57589abb45d2280b37b230023f252f765d1e1ef
江韬 <tianjiuweixun@gmail.com>
2025-07-18 14:52:21 +0800

    添加部署脚本

--- vite.config.js
+++ vite.config.js
@@ -2,17 +2,20 @@ import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // 这是最重要的部分！
+  // base 的值应该是 "/<你的仓库名>/"
+  // 例如，如果你的仓库是 "sweetness-energy-station"，就填 "/sweetness-energy-station/"
+  base: '/sweetness-energy-station/', 
+
   plugins: [
     vue(),
-    vueDevTools(),
   ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
-    },
-  },
+    }
+  }
 })
